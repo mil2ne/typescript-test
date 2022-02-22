df235d9471c1a15efed422549907f9990d9916c4
mil2ne <show3070@gmail.com>
2022-02-23 06:52:03 +0900

    typescript tuple

---
+++ tuple.ts
@@ -0,0 +1,12 @@
+let x: [string, number];
+
+x = ["hello", 20];
+
+// x = [10, 'mark']; // error
+
+// x[2] = "world"; // error
+
+const person: [string, number] = ["Mark", 39];
+
+const [first, second] = person;
+
