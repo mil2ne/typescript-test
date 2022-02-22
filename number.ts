4d4ceab9f0611d1b598f1aa7ab3b18f616b3d9ca
mil2ne <show3070@gmail.com>
2022-02-22 19:50:53 +0900

    typescript number

---
+++ number.ts
@@ -0,0 +1,11 @@
+let decimal: number = 10;
+
+let hex: number = 0xf00d;
+
+let binary: number = 0b1010;
+
+let octal: number = 0o744;
+
+let notANumber: number = NaN;
+
+let undersoreNum: number = 1_000_000;
